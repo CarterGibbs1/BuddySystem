881a7bc6291a0ce09ed96473c6d917f5d68599c2
CarterGibbs1 <97470347+CarterGibbs1@users.noreply.github.com>
2024-11-30 18:45:42 +0000

    Added starter code

--- src/lab.c
+++ src/lab.c
@@ -1,1 +1,33 @@
-/**Update this file with the starter code**/
+#include <stddef.h>
+
+size_t btok(size_t bytes) {
+
+}
+
+struct avail *buddy_calc(struct buddy_pool *pool, struct avail *buddy) {
+
+}
+
+void *buddy_malloc(struct buddy_pool *pool, size_t size) {
+
+}
+
+void buddy_free(struct buddy_pool *pool, void *ptr) {
+
+}
+
+void *buddy_realloc(struct buddy_pool *pool, void *ptr, size_t size) {
+
+}
+
+void buddy_init(struct buddy_pool *pool, size_t size) {
+
+}
+
+void buddy_destroy(struct buddy_pool *pool) {
+
+}
+
+int myMain(int argc, char** argv) {
+
+}
